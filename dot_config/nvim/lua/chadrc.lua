a24dd929cbe51e29fa0e5912d5688bf3106d7458
kevinmhk <kevinmhk@gmail.com>
2026-01-15 01:42:40 +0800

    Update .config/nvim/lua/chadrc.lua

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -20,12 +20,29 @@ M.base46 = {
 --          lazyload = false
 --      }
 -- }
-
+  local sep_style = "default"
+  local utils = require "nvchad.stl.utils"
+  local sep_icons = utils.separators
+  local seps = (type(sep_style) == "table" and sep_style) or sep_icons[sep_style]
+  local sep_l = seps["left"]
   M.ui = {
     statusline = {
       theme = "default",
       separator_style="default",
-      order={"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor"}
+      order={"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "encoding","cwd", "cursor"},
+      modules={
+      cursor = table.concat({
+        "%#St_pos_sep#",  sep_l,
+        "%#St_pos_icon#", "  ",
+        "%#St_pos_text#", " %l:%c  %p%% ",
+      }),
+      encoding = table.concat({
+        "%#St_pos_sep#",  sep_l,
+        "%#St_pos_icon#", "  ",
+        "%#St_pos_text#",
+        " %{&fileencoding != '' ? &fileencoding : &encoding} ",
+      })
+    }
   }
 }
 
