6d7693b98141cfa05b90f1c9f03d6a325c43e75a
kevinmhk <kevinmhk@gmail.com>
2026-01-15 01:23:11 +0800

    Update .config/nvim/lua/chadrc.lua

--- dot_config/nvim/lua/chadrc.lua
+++ dot_config/nvim/lua/chadrc.lua
@@ -21,4 +21,12 @@ M.base46 = {
 --      }
 -- }
 
+  M.ui = {
+    statusline = {
+      theme = "default",
+      separator_style="default",
+      order={"mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor"}
+  }
+}
+
 return M
